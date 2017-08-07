21865a081d4e6322c8388f5199f923f413bd9a46
Jennifer Wang <Jwang28@gmail.com>
2017-08-07 07:57:33 -0700

    Add background scroll feature partially complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  host: {
+  	'(window:scroll)': 'execute($event)'
+  }
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private sanitizer: DomSanitizer) { }
+  setSpeed: number = 100;
+	speed: any;
+	scrollY: any;
+	bgPos: any;
   ngOnInit() {
+  }
+
+  execute(evt){
+    console.log("scrollTop is: " + evt.target.scrollTop);
+    console.log(window.scrollY.valueOf());
+    /*this.scrollY = window.scrollY.valueOf();
+    if (this.scrollY === 0){
+      this.speed = 0;
+    }
+    else{
+      this.speed = this.scrollY/this.setSpeed;
+    }
+    console.log("speed is: " + this.speed);
+    this.bgPos = this.sanitizer.bypassSecurityTrustStyle("0%  "+ this.speed+ "%"); 
+    console.log(this.bgPos);
+*/
   }
 
 }
--- src/app/alumni/alumni.component.ts
+++ src/app/alumni/alumni.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-alumni',
   templateUrl: './alumni.component.html',
-  styleUrls: ['./alumni.component.css']
+  styleUrls: ['./alumni.component.css'],
+  host: {
+  	'(window:scroll)': 'execute($event)'
+  }
 })
 export class AlumniComponent implements OnInit {
+	constructor(private sanitizer: DomSanitizer) { }
+	  setSpeed: number = 2;
+		top1: any;
+		top2:any;
+		scrollY: any;
+		start1: number = -100;
+		start2: number= -100;
+		end1: any;
+		end2: any;
+		
+	  ngOnInit() {
+	  }
 
-  constructor() { }
+	  execute(evt){
+	  	
+	    console.log("scrollTop is: " + document.body.scrollTop);
 
-  ngOnInit() {
-  }
+	    console.log("scroll Y is: " + window.scrollY.valueOf());
+	    
+	    this.scrollY = window.scrollY.valueOf() % window.innerHeight.valueOf();
+	   	
+
+	    this.top1 = this.start1 + this.scrollY/this.setSpeed;
+	    this.top2 = this.start1 + this.scrollY/this.setSpeed;
+
+	    //console.log("top is: " + this.top1);
+	    this.end1 = this.sanitizer.bypassSecurityTrustStyle(this.top1 + "px"); 
+	    this.end2 = this.sanitizer.bypassSecurityTrustStyle(this.top2 + "px"); 
+	    //console.log(this.end1);
+	
+	  }
+
 
 }
+
