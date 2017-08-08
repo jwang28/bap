3b764128b7efbadb6007532edb643ae64f7245b9
Jennifer Wang <Jwang28@gmail.com>
2017-08-08 07:55:09 -0700

    Fix background parallax scroll

--- src/app/alumni/alumni.component.ts
+++ src/app/alumni/alumni.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -10,36 +10,53 @@ import { DomSanitizer } from '@angular/platform-browser';
   }
 })
 export class AlumniComponent implements OnInit {
-	constructor(private sanitizer: DomSanitizer) { }
-	  setSpeed: number = 2;
+	 
+
+	constructor(private sanitizer: DomSanitizer, private el:ElementRef) { }
+	  setSpeed: number = 1.5;
 		top1: any;
-		top2:any;
+		top2: any=0;
 		scrollY: any;
-		start1: number = -100;
-		start2: number= -100;
+		start1: number = 0;
+		start2: number = 0;
 		end1: any;
 		end2: any;
+		height: any;
+		offset: any;
+		@ViewChild('test') ele: ElementRef;
+		@ViewChild('test2') ele2: ElementRef;
 		
 	  ngOnInit() {
+	  	console.log(this.ele.nativeElement.getBoundingClientRect().top);
+	  	console.log(this.ele2.nativeElement.getBoundingClientRect().top);
+	  	/*this.start2 = this.ele2.nativeElement.getBoundingClientRect().top;*/
 	  }
 
 	  execute(evt){
 	  	
-	    console.log("scrollTop is: " + document.body.scrollTop);
+	   /* console.log("scrollTop is: " + document.body.scrollTop);
 
-	    console.log("scroll Y is: " + window.scrollY.valueOf());
-	    
+	    console.log("scroll Y is: " + window.scrollY.valueOf());*/
+
+	    this.height = window.innerHeight;
+	    console.log("ele2: " + this.ele2.nativeElement.getBoundingClientRect().top + " " + window.innerHeight);
+
 	    this.scrollY = window.scrollY.valueOf() % window.innerHeight.valueOf();
 	   	
 
-	    this.top1 = this.start1 + this.scrollY/this.setSpeed;
-	    this.top2 = this.start1 + this.scrollY/this.setSpeed;
+	    this.top1 = -this.scrollY/this.setSpeed;
 
+	    this.offset = (this.ele2.nativeElement.getBoundingClientRect().bottom < 0) ? 0 : (window.innerHeight - this.ele2.nativeElement.getBoundingClientRect().top);
+
+	    console.log(this.offset);
+	   	if((window.innerHeight -  this.ele2.nativeElement.getBoundingClientRect().top) > 0){
+	    	this.top2 = 200-this.offset*0.5;
+	    	console.log("reset");
+	    }
+	    
 	    //console.log("top is: " + this.top1);
 	    this.end1 = this.sanitizer.bypassSecurityTrustStyle(this.top1 + "px"); 
 	    this.end2 = this.sanitizer.bypassSecurityTrustStyle(this.top2 + "px"); 
-	    //console.log(this.end1);
-	
 	  }
 
 
