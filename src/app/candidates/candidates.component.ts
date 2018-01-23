6b783a793543ab2c69d4f9e6cc298fe43e4daf7a
Jennifer Wang <Jwang28@gmail.com>
2018-01-23 16:44:58 -0500

    Change both links

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -27,7 +27,7 @@ export class CandidatesComponent implements OnInit {
 
   redirect(){
   	/*window.location.href = 'https://goo.gl/forms/Nft2PkKtiKZjh8Yo2';*/
-    window.open('https://docs.google.com/document/d/1qb0FcCNd25YYTFp2Nvo5edGXgBFOqt72vnV75XCnBT0/edit');
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSfqc-C_kMPlo-F1xS0dige7eotnGeR1E_y2XAKQWholfo8vDA/viewform');
   }
 
   toLogin(){
